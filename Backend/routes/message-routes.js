01d0a2548c5702e1c8ed3a15244037ec00326255
Jeremy Marino <jermarino16@gmail.com>
2020-07-24 08:24:06 -0700

    setup message routes with no logic

--- Backend/routes/message-routes.js
+++ Backend/routes/message-routes.js
@@ -1,12 +1,51 @@
 const express = require("express");
 
-const messagesControllers = require("../controllers/message-controllers");
+const messageControllers = require("../controllers/message-controllers");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("Get request in users");
-  res.json({ message: "Message get `/` works" });
+router.get("/user/:uid", (req, res, next) => {
+  res.json({ message: "Shows all messages that a user has polled." });
+});
+
+router.get("/new", (req, res, next) => {
+  res.json({
+    message: "Shows the create message form for a Queue",
+  });
+});
+
+router.get("/:messageID", (req, res, next) => {
+  res.json({
+    message:
+      "Display one message based on message ID. Messages can only be read have to be by users with associated user ID's.",
+  });
+});
+
+router.post("/:messageID", (req, res, next) => {
+  res.json({
+    message:
+      "Creates a message based on body content. Returns the message object created and wil add to available messages to the Queue with associated ID. Only admins can create messages.",
+  });
+});
+
+router.patch("/:messageID", (req, res, next) => {
+  res.json({
+    message:
+      "Updates a message based on body content, and userID. Returns updated message in response. Only admin's can update messages. ",
+  });
+});
+
+router.delete("/:messageID", (req, res, next) => {
+  res.json({
+    message:
+      "Deletes a message based on message ID. Only an admin can delete messages.",
+  });
+});
+
+router.get("/:messageID/edit", (req, res, next) => {
+  res.json({
+    message: "Display edit form of one message",
+  });
 });
 
 module.exports = router;
