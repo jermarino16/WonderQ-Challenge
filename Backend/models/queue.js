2066c2adf818747531cc60a5bfaaedd267b565c9
Jeremy Marino <jermarino16@gmail.com>
2020-07-24 09:46:13 -0700

    changed model for dummy data

--- Backend/models/queue.js
+++ Backend/models/queue.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const queueSchema = new Schema({
   availableMessages: [{ type: mongoose.Types.ObjectId, ref: "Message" }],
+  queueID: { type: String, required: true }, //Will be a unique identifier but using a number for dummy data
   name: { type: String, required: true },
   messageRetentionTime: { type: Number },
   maxMessageCount: { type: Number },
