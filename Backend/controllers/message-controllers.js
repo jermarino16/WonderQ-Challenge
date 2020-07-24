3e6c91700b86ba48c5cc7f7c3b1a31361bdb5efe
Jeremy Marino <jermarino16@gmail.com>
2020-07-24 13:30:24 -0700

    logic setup for getting message by UID  and  QID

--- Backend/controllers/message-controllers.js
+++ Backend/controllers/message-controllers.js
@@ -5,29 +5,40 @@ const Queue = require("../models/queue");
 
 let DUMMY_MESSAGES = [
   {
-    content: "Here is dummy content",
-    messageID: "1",
+    content: "Here is dummy content for q1 u1",
+    messageID: "10",
     queueID: "1",
     currentUserID: "1",
   },
   {
-    content: "Here is more dummy content",
-    messageID: "2",
+    content: "Dummy content for q1 u1",
+    messageID: "20",
+    queueID: "1",
+    currentUserID: "1",
+  },
+  {
+    content: "Dummy content for q2 u2",
+    messageID: "30",
     queueID: "2",
     currentUserID: "2",
   },
 ];
 
 const getMessagesByUserIDAndQueueID = async (req, res, next) => {
-  res.json(req.params);
   const { userID, queueID } = req.params;
-  const userMessages = DUMMY_MESSAGES.filter((m) => {
-    m.currentUserID === userID && m.queueID === queueID;
-  });
+
+  const userMessages = DUMMY_MESSAGES.filter(
+    (m) => m.currentUserID === userID && m.queueID === queueID
+  );
+  //validation checks can be done to see if the queue is valid,  or user id is valid
+  //i will only check for messages
+  if (!userMessages.length) {
+    res.json({
+      message: "Sorry there are no messages for that user or that queue",
+    });
+  }
 
   res.json({
-    userID,
-    queueID,
     messages: userMessages,
     message: `Here are all messages from queue: ${queueID} that match the user ID: ${userID}`,
   });
