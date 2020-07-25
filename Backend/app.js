330f7f2eea76fa799b39db26c0c6d1c54be5dac2
Jeremy Marino <jermarino16@gmail.com>
2020-07-24 17:13:15 -0700

    headers

--- Backend/app.js
+++ Backend/app.js
@@ -11,6 +11,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+//HEADERS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
 //ROUTES
 app.use("/api/users", usersRoutes); //not used for this project
 app.use("/api/queues", queueRoutes);
